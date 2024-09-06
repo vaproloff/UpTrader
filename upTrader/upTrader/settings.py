d345445eaf1c47da00ab20480f87af3b888b584e
Konstantin Pivnov <vaproloff@ya.ru>
2024-09-06 16:50:50 +0300

    turning Debug = True

--- upTrader/upTrader/settings.py
+++ upTrader/upTrader/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-4@wz+8s-kdngm05wowmr2r865#8e!v!45@96ogg*a)7b1mv#ik"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
